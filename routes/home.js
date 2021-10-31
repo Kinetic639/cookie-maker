24182852d89acf8cc9866db6d40a1068502ef7c1
Kinetic639 <michal.stepien36@gmail.com>
2021-10-31 10:45:04 +0100

    display cookies and bases, created database for addons and bases, partials displaying all addons and bases

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const { COOKIE_BASES, COOKIE_ADDONS } = require('../data/cookies-data');
 
 const homeRouter = express.Router();
 
 homeRouter.get('/', (req, res) => {
-  res.render('home/index');
+  res.render('home/index', {
+    cookie: {
+      base: 'light',
+      addons: ['coconut', 'cranberries', 'honey'],
+    },
+    bases: Object.entries(COOKIE_BASES),
+    addons: Object.entries(COOKIE_ADDONS),
+    // TODO: we need something to know what are the prices of selected items
+  });
 });
 
 module.exports = {
--- routes/order.js
+++ routes/order.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const orderRouter = express.Router();
 
-orderRouter;
+// orderRouter;
 
 module.exports = {
   orderRouter,
