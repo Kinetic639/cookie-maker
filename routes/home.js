e1298860f3a76218237817dbf58cf224f083da78
Kinetic639 <michal.stepien36@gmail.com>
2021-11-01 15:03:23 +0100

    count and show total price of a cookie, added handlebar helper for displaying price with two decimals

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const { COOKIE_BASES, COOKIE_ADDONS } = require('../data/cookies-data');
+const { handlebarsHelpers } = require('../handlebars-helpers');
 
 const homeRouter = express.Router();
 
 homeRouter.get('/', (req, res) => {
+  const sum =
+    handlebarsHelpers.findPrice(Object.entries(COOKIE_BASES), 'light') +
+    ['coconut', 'honey'].reduce(
+      (prev, curr) =>
+        prev + handlebarsHelpers.findPrice(Object.entries(COOKIE_ADDONS), curr),
+      0,
+    );
+
   res.render('home/index', {
     cookie: {
       base: 'light',
@@ -11,7 +20,7 @@ homeRouter.get('/', (req, res) => {
     },
     bases: Object.entries(COOKIE_BASES),
     addons: Object.entries(COOKIE_ADDONS),
-
+    sum,
     // TODO: we need something to know what are the prices of selected items
   });
 });
