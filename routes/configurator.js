80983542e5ba0eb2c7bf2805880ac0d18dc88a42
Kinetic639 <michal.stepien36@gmail.com>
2021-11-03 23:13:37 +0100

    removing addons from basket and database

--- routes/configurator.js
+++ routes/configurator.js
@@ -3,20 +3,31 @@ const { getAddonsFromReq } = require('../utils/get-addons-from-req');
 
 const configuratorRouter = express.Router();
 
-configuratorRouter.get('/select/base/:base', (req, res) => {
-  const { base } = req.params;
-  res.cookie('cookieBase', base).render('configurator/base-selected', { base });
-});
+configuratorRouter
+  .get('/select/base/:base', (req, res) => {
+    const { base } = req.params;
+    res
+      .cookie('cookieBase', base)
+      .render('configurator/base-selected', { base });
+  })
+  .get('/select/addon/:addon', (req, res) => {
+    const { addon } = req.params;
 
-configuratorRouter.get('/select/addon/:addon', (req, res) => {
-  const { addon } = req.params;
+    const addons = getAddonsFromReq(req);
+    addons.push(addon);
+    res
+      .cookie('cookieAddons', JSON.stringify(addons))
+      .render('configurator/added', { addon });
+  })
+  .get('/remove/addon/:addon', (req, res) => {
+    const { addon } = req.params;
 
-  const addons = getAddonsFromReq(req);
-  addons.push(addon);
-  res
-    .cookie('cookieAddons', JSON.stringify(addons))
-    .render('configurator/added', { addon });
-});
+    const addons = getAddonsFromReq(req).filter((add) => add !== addon);
+
+    res
+      .cookie('cookieAddons', JSON.stringify(addons))
+      .render('configurator/removed', { addon });
+  });
 
 module.exports = {
   configuratorRouter,
