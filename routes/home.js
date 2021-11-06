0b78c9b4d7ab1936ce63e47699468008746a3aba
Kinetic639 <michal.stepien36@gmail.com>
2021-11-06 18:09:04 +0100

    small refactoring code

--- routes/home.js
+++ routes/home.js
@@ -1,37 +1,21 @@
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable operator-linebreak */
 const express = require('express');
-const { COOKIE_BASES, COOKIE_ADDONS } = require('../data/cookies-data');
-const { getAddonsFromReq } = require('../utils/get-addons-from-req');
-const { handlebarsHelpers } = require('../utils/handlebars-helpers');
+const { getCookieSettings } = require('../utils/get-cookie-setting');
 
 const homeRouter = express.Router();
 
 homeRouter.get('/', (req, res) => {
-  const { cookieBase } = req.cookies;
-
-  const addons = getAddonsFromReq(req);
-
-  const sum =
-    handlebarsHelpers.findPrice(
-      Object.entries(COOKIE_BASES),
-      cookieBase || 'light',
-    ) +
-    addons.reduce(
-      (prev, curr) =>
-        prev + handlebarsHelpers.findPrice(Object.entries(COOKIE_ADDONS), curr),
-      0,
-    );
+  const { addons, sum, base, allBases, allAddons } = getCookieSettings(req);
 
   res.render('home/index', {
     cookie: {
-      base: cookieBase || 'light',
+      base,
       addons,
     },
-    bases: Object.entries(COOKIE_BASES),
-    addons: Object.entries(COOKIE_ADDONS),
+    allBases,
+    allAddons,
     sum,
-    // TODO: we need something to know what are the prices of selected items
   });
 });
 
--- routes/order.js
+++ routes/order.js
@@ -1,54 +1,25 @@
 const express = require('express');
-const { COOKIE_BASES, COOKIE_ADDONS } = require('../data/cookies-data');
-const { getAddonsFromReq } = require('../utils/get-addons-from-req');
-const { handlebarsHelpers } = require('../utils/handlebars-helpers');
+const { getCookieSettings } = require('../utils/get-cookie-setting');
 
 const orderRouter = express.Router();
 
 orderRouter
   .get('/summary', (req, res) => {
-    const { cookieBase } = req.cookies;
-
-    const addons = getAddonsFromReq(req);
-
-    const sum =
-      handlebarsHelpers.findPrice(
-        Object.entries(COOKIE_BASES),
-        cookieBase || 'light',
-      ) +
-      addons.reduce(
-        (prev, curr) =>
-          prev +
-          handlebarsHelpers.findPrice(Object.entries(COOKIE_ADDONS), curr),
-        0,
-      );
+    const { addons, sum, base, allBases, allAddons } = getCookieSettings(req);
 
     res.render('order/summary', {
       cookie: {
-        base: cookieBase || 'light',
+        base,
         addons,
       },
-      bases: Object.entries(COOKIE_BASES),
-      addons: Object.entries(COOKIE_ADDONS),
+      allBases,
+      allAddons,
       sum,
     });
   })
+
   .get('/thanks', (req, res) => {
-    const { cookieBase } = req.cookies;
-
-    const addons = getAddonsFromReq(req);
-
-    const sum =
-      handlebarsHelpers.findPrice(
-        Object.entries(COOKIE_BASES),
-        cookieBase || 'light',
-      ) +
-      addons.reduce(
-        (prev, curr) =>
-          prev +
-          handlebarsHelpers.findPrice(Object.entries(COOKIE_ADDONS), curr),
-        0,
-      );
+    const { sum } = getCookieSettings(req);
     res
       .clearCookie('cookieBase')
       .clearCookie('cookieAddons')
